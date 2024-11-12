d59493c83096cc171fd2d2fcb25feca7984fdf6d
Nghia Tran <80335335+twilightty@users.noreply.github.com>
2024-11-12 15:01:36 +0700

    Update pdf.worker.entry.ts

--- packages/react-pdf/src/pdf.worker.entry.ts
+++ packages/react-pdf/src/pdf.worker.entry.ts
@@ -5,6 +5,20 @@
  * this bundle, not theirs.
  */
 
+// @ts-expect-error This does not exist outside of polyfill which this is doing
+if (typeof Promise.withResolvers === 'undefined') {
+  if (window)
+      // @ts-expect-error This does not exist outside of polyfill which this is doing
+      window.Promise.withResolvers = function () {
+          let resolve, reject;
+          const promise = new Promise((res, rej) => {
+              resolve = res;
+              reject = rej;
+          });
+          return { promise, resolve, reject };
+      };
+}
+
 (
   (typeof window !== 'undefined' ? window : {}) as Window &
     typeof globalThis & { pdfjsWorker: unknown }
